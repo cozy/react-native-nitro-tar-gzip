537046774455791c52613d57457ac9f1c5bcc19a
Ldoppea <yannick.chiron@gmail.com>
2025-03-18 15:00:38 +0100

    feat(common): Add specs and implementation

--- react-native-nitro-gzip/src/index.ts
+++ react-native-nitro-gzip/src/index.ts
@@ -1,1 +1,63 @@
-// TODO: Export all HybridObjects here for the user
+import { NitroModules } from 'react-native-nitro-modules'
+import type {
+  NitroGzip,
+  GzipResult as NativeResult,
+} from './specs/NitroGzip.nitro'
+
+const GzipHybridObject = NitroModules.createHybridObject<NitroGzip>('NitroGzip')
+
+interface GzipResultSuccess {
+  success: true
+  path: string
+}
+
+interface GzipResultFail {
+  success: false
+  reason: string
+}
+
+type GzipResult = GzipResultSuccess | GzipResultFail
+
+const wrapResult = (result: NativeResult): GzipResult => {
+  if (result.success && result.path) {
+    return {
+      success: true,
+      path: result.path,
+    }
+  } else {
+    return {
+      success: false,
+      reason: result.error || 'unkown error',
+    }
+  }
+}
+
+export async function unTar(
+  sourcePath: string,
+  targetPath: string,
+  force: boolean
+): Promise<GzipResult> {
+  const result = await GzipHybridObject.unTar(sourcePath, targetPath, force)
+
+  return wrapResult(result)
+}
+
+export async function unGzip(
+  sourcePath: string,
+  targetPath: string,
+  force: boolean
+): Promise<GzipResult> {
+  const result = await GzipHybridObject.unGzip(sourcePath, targetPath, force)
+
+  return wrapResult(result)
+}
+
+export async function unGzipTar(
+  sourcePath: string,
+  targetPath: string,
+  force: boolean
+): Promise<GzipResult> {
+  const result = await GzipHybridObject.unGzipTar(sourcePath, targetPath, force)
+
+  return wrapResult(result)
+}
