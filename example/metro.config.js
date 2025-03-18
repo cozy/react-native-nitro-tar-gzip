10adcf0708c2ff8cf0c90103335341ebc777c26f
Ldoppea <yannick.chiron@gmail.com>
2025-03-18 14:53:54 +0100

    feat(example): Configure example project to build from a yarn workspace
    Because the module's code is not not on the project's root, we need to
    tell react-native gradle config how to access node_modules files

--- example/metro.config.js
+++ example/metro.config.js
@@ -1,4 +1,6 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+const path = require('path');
+const root = path.resolve(__dirname, '..');
 
 /**
  * Metro configuration
@@ -6,6 +8,8 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
  *
  * @type {import('@react-native/metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+  watchFolders: [root]
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
