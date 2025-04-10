580ee8de7a676c33e48f9e4b9e13562aec2e6f4a
Ldoppea <yannick.chiron@gmail.com>
2025-04-10 15:49:09 +0200

    chore: Add release-it configuration

---
+++ scripts/release.sh
@@ -0,0 +1,12 @@
+echo "Starting the release process..."
+echo "Provided options: $@"
+
+echo "Publishing react-native-nitro-gzip to NPM"
+cd package
+yarn release $@
+
+echo "Creating a Git bump commit and GitHub release"
+cd ..
+yarn run release-it $@
+
+echo "Successfully released react-native-nitro-gzip!"
